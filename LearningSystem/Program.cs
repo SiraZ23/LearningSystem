35b7ab8d2d1abb4026d5f5088d9207d2504696fc
siraz17 <olisiraj251@gmail.com>
2025-01-02 18:41:43 +0545

    Revert "3rd  Modification On Console.WriteLine"
    This reverts commit c89fd122aeb4f0b18b392be9939ac8f0da8f9aa0.

--- LearningSystem/Program.cs
+++ LearningSystem/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Programming! One More Modification"); 
+Console.WriteLine("Hello, C# Programming! One More Modification"); Console.WriteLine("Hello, C# Programming! One More Modification");
+Console.WriteLine("Hello, C# Programming! One More Modification"); Console.WriteLine("Hello, C# Programming! One More Modification");
+Console.WriteLine("Hello, C# Programming! One More Modification"); Console.WriteLine("Hello, C# Programming! One More Modification");
+Console.WriteLine("Hello, C# Programming! One More Modification");
