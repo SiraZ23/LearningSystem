c89fd122aeb4f0b18b392be9939ac8f0da8f9aa0
siraz17 <138348695+SiraZ23@users.noreply.github.com>
2025-01-02 18:38:10 +0545

    3rd  Modification On Console.WriteLine

--- LearningSystem/Program.cs
+++ LearningSystem/Program.cs
@@ -1,5 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Programming! One More Modification"); Console.WriteLine("Hello, C# Programming! One More Modification");
-Console.WriteLine("Hello, C# Programming! One More Modification"); Console.WriteLine("Hello, C# Programming! One More Modification");
-Console.WriteLine("Hello, C# Programming! One More Modification"); Console.WriteLine("Hello, C# Programming! One More Modification");
-Console.WriteLine("Hello, C# Programming! One More Modification");
+Console.WriteLine("Hello, C# Programming! One More Modification"); 
